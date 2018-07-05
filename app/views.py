18febb6ae62453f115bce4b5d08834ecc6c64ec4
Thelonius Kort <tnt@users.noreply.github.com>
2018-07-05 19:12:14 +0200

    just serving some files for local testing purposes

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template,send_from_directory
 
 from app import app
 
@@ -11,3 +11,7 @@ def index():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/<path:path>')
+def send_js(path):
+    return send_from_directory('/c/Users/thelonius/git/testunk', path)
